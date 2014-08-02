1da1046aed033ea0258cf573413e3a6dea921ecb
Jason McVetta <jason.mcvetta@gmail.com>
2014-08-01 17:57:12 -0700

    rate limit error

--- rapportive.go
+++ rapportive.go
@@ -3,20 +3,19 @@
 package rapportive
 
 import (
-	"github.com/jmcvetta/napping"
-	"log"
-	"runtime"
-	"strconv"
-	"github.com/kr/pretty"
 	"errors"
 	"fmt"
+	"github.com/jmcvetta/napping"
+	"github.com/kr/pretty"
+	"log"
 	"net/http"
+	"runtime"
+	"strconv"
 )
 
 const (
-	statusUrl = "https://rapportive.com/login_status?user_email=%s"
+	statusUrl   = "https://rapportive.com/login_status?user_email=%s"
 	contactsUrl = "https://profiles.rapportive.com/contacts/email/%s"
-
 )
 
 type loginResult struct {
@@ -25,33 +24,32 @@ type loginResult struct {
 }
 
 type contactsResult struct {
-	Name string
+	Name    string
 	Contact Contact
 }
 
 type Contact struct {
-	Name string
-	Email string
-	Twitter string `json:"twitter_username"`
-	Location string
-	Headline string
+	Name        string
+	Email       string
+	Twitter     string `json:"twitter_username"`
+	Location    string
+	Headline    string
 	Occupations []*Occupation
 	Memberships []*Membership
 }
 
 type Occupation struct {
 	JobTitle string `json:"job_title"`
-	Company string
+	Company  string
 }
 
 type Membership struct {
-	Site string `json:"site_name"`
-	Username string
+	Site       string `json:"site_name"`
+	Username   string
 	ProfileUrl string `json:"profile_url"`
 }
 
-
-func login(email string) (sessionToken string, err error){
+func login(email string) (sessionToken string, err error) {
 	p := napping.Params{
 		"user_email": email,
 	}
@@ -86,6 +84,8 @@ func Query(email string) (c *Contact, err error) {
 	return
 }
 
+var RateLimitError = errors.New("Rate Limit Error")
+
 func getContact(sessionToken, email string) (*Contact, error) {
 	h := http.Header{}
 	h.Add("X-Session-Token", sessionToken)
@@ -99,6 +99,10 @@ func getContact(sessionToken, email string) (*Contact, error) {
 		log.Println(err)
 		return nil, err
 	}
+	if resp.Status() == 429 {
+		log.Println(RateLimitError)
+		return nil, RateLimitError
+	}
 	if resp.Status() != 200 {
 		msg := fmt.Sprintf("Bad response status: %v", resp.Status())
 		err = errors.New(msg)
@@ -107,7 +111,6 @@ func getContact(sessionToken, email string) (*Contact, error) {
 	}
 	return &result.Contact, nil
 }
-
 
 func logPretty(x interface{}) {
 	_, file, line, _ := runtime.Caller(1)
