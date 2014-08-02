2c39616614acb2049a8257ee75b58acb47e12c06
Jason McVetta <jason.mcvetta@gmail.com>
2014-08-01 18:36:33 -0700

    gplv3

--- rapportive.go
+++ rapportive.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2014 Jason McVetta.  This is Free Software, released under
+// the terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for
+// details.  Resist intellectual serfdom - the ownership of ideas is akin to
+// slavery.
+
 // Package rapportive is a client for the undocumented Rapportive API.
 // Inspired by:  https://github.com/jordan-wright/rapportive
 package rapportive
